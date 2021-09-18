daa3a60ee9f3f00879afa64501844080d4e82a91
Faizul Haq <cs19mtech01002@iith.ac.in>
2021-09-19 01:35:34 +0530

    Time: 4 ms (90.77%), Space: 13.8 MB (22.38%) - LeetHub

--- min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -7,12 +7,12 @@ public:
         mc.push_back(0);
         
         
-        for(int i = 2; i < cost.size(); i++)
+        for(int i = 2; i <= cost.size(); i++)
         {
             mc.push_back(min(mc[i-1]+cost[i-1], mc[i-2]+cost[i-2]));
         }
-        int l = mc.size(); 
-        mc.push_back(min(mc[l-1]+cost[l-1], mc[l-2]+cost[l-2]));
+        // int l = mc.size(); 
+        // mc.push_back(min(mc[l-1]+cost[l-1], mc[l-2]+cost[l-2]));
         return mc.back();
         
     }
